507c793446750db4e8b2abb379c057ba06c6dd58
NguyenZuy <duy.nguyen@smulie.io>
2025-11-18 18:06:18 +0700

    update

---
+++ problem_list/4_median_of_two_sorted_arrays/solution.cpp
@@ -0,0 +1,27 @@
+/*
+De Bai:
+Cho 2 mang da sap xep `nums1` va `nums2` co size `m` va `n`, tim va tra ve trung vi cua hai mang nay
+
+Rang buoc:
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 10^6
+
+Sliding window
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+    }
+};
